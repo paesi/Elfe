8607293d6bb8847274bb374304660f68fa6ecd53
paesi <97041237+paesi@users.noreply.github.com>
2024-12-23 07:45:29 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Hero from './components/Hero';
@@ -9,29 +10,33 @@ import Contact from './components/Contact';
 import Imprint from './pages/Imprint';
 import Privacy from './pages/Privacy';
 import Terms from './pages/Terms';
+import { SEO } from './components/SEO';
 
 export default function App() {
   return (
-    <Router>
-      <div className="min-h-screen flex flex-col">
-        <Header />
-        
-        <Routes>
-          <Route path="/" element={
-            <main className="flex-grow pt-[104px]">
-              <Hero />
-              <ServicesSection />
-              <WhyChooseUs />
-              <Contact />
-            </main>
-          } />
-          <Route path="/imprint" element={<Imprint />} />
-          <Route path="/privacy" element={<Privacy />} />
-          <Route path="/terms" element={<Terms />} />
-        </Routes>
+    <HelmetProvider>
+      <Router>
+        <div className="min-h-screen flex flex-col">
+          <SEO />
+          <Header />
+          
+          <Routes>
+            <Route path="/" element={
+              <main className="flex-grow pt-[104px]">
+                <Hero />
+                <ServicesSection />
+                <WhyChooseUs />
+                <Contact />
+              </main>
+            } />
+            <Route path="/imprint" element={<Imprint />} />
+            <Route path="/privacy" element={<Privacy />} />
+            <Route path="/terms" element={<Terms />} />
+          </Routes>
 
-        <Footer />
-      </div>
-    </Router>
+          <Footer />
+        </div>
+      </Router>
+    </HelmetProvider>
   );
 }
