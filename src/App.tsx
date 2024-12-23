11b1f74469819ef4f42e620ce159ab02503bafb2
paesi <97041237+paesi@users.noreply.github.com>
2024-12-23 07:18:10 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,36 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Hero from './components/Hero';
 import ServicesSection from './components/ServicesSection';
 import WhyChooseUs from './components/WhyChooseUs';
+import References from './components/References';
 import Contact from './components/Contact';
-import PrivacyPolicy from './components/PrivacyPolicy';
-import Imprint from './components/Imprint';
-import Terms from './components/Terms';
+import Imprint from './pages/Imprint';
+import Privacy from './pages/Privacy';
+import Terms from './pages/Terms';
 
 export default function App() {
   return (
     <Router>
       <div className="min-h-screen flex flex-col">
         <Header />
-
-        <main className="flex-grow pt-[104px]">
-          <Routes>
-            <Route path="/" element={
-              <>
-                <Hero />
-                <ServicesSection />
-                <WhyChooseUs />
-                <Contact />
-              </>
-            } />
-            <Route path="/privacy" element={<PrivacyPolicy />} />
-            <Route path="/imprint" element={<Imprint />} />
-            <Route path="/terms" element={<Terms />} />
-          </Routes>
-        </main>
+        
+        <Routes>
+          <Route path="/" element={
+            <main className="flex-grow pt-[104px]">
+              <Hero />
+              <ServicesSection />
+              <WhyChooseUs />
+              <References />
+              <Contact />
+            </main>
+          } />
+          <Route path="/imprint" element={<Imprint />} />
+          <Route path="/privacy" element={<Privacy />} />
+          <Route path="/terms" element={<Terms />} />
+        </Routes>
 
         <Footer />
       </div>
