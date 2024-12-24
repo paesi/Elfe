c586822b617fcc0a28ee814c1b81fbb45d034b3d
paesi <97041237+paesi@users.noreply.github.com>
2024-12-24 09:42:15 +0100

    Update data.ts

--- src/components/configurator/data.ts
+++ src/components/configurator/data.ts
@@ -1,15 +1,5 @@
 import { ServiceOption, PricingFactors } from './types';
 
-export const services: ServiceOption[] = [
-  {
-    id: 'basic-cleaning',
-    name: 'Grundreinigung',
-    description: 'Regelmäßige Reinigung von Böden, Oberflächen und Sanitäranlagen',
-    basePrice: 3.5,
-    unit: 'm²',
-    type: 'cleaning'
-  },
-];
 
 export const frequencyFactors: PricingFactors = {
   weekly: 0.9,    // 10% Rabatt
