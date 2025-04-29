cf4d8454518eb2cc0b1cd256e4be4fbc8b8076e2
paesi <97041237+paesi@users.noreply.github.com>
2025-04-29 21:29:56 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ export default {
       colors: {
         primary: '#2C3E50',      // Logo-Blaugrau
         secondary: '#4A4A4A',    // Für Texte, Icons
-        background: '#F0F8F7',   // Logo-Hintergrundfarbe
+        background: '#e1eae9',   // Logo-Hintergrundfarbe
         highlight: '#A8DADC',    // Frischer Akzent (z. B. für Positionen, Buttons)
       },
     },
