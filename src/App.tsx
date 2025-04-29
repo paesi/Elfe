d747ca64942213895f367f0c0e02af393d86a6ac
paesi <97041237+paesi@users.noreply.github.com>
2025-04-29 20:16:24 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import Hero from './components/Hero';
 import ServicesSection from './components/ServicesSection';
 import WhyChooseUs from './components/WhyChooseUs';
 import Contact from './components/Contact';
-import Configurator from './components/configurator/Configurator';
 import Imprint from './pages/Imprint';
 import Privacy from './pages/Privacy';
 import Terms from './pages/Terms';
@@ -29,7 +28,6 @@ export default function App() {
               <main className="flex-grow pt-[104px]">
                 <Hero />
                 <ServicesSection />
-                <Configurator />
                 <WhyChooseUs />
                 <Contact />
               </main>
