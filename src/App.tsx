8b1bad82108046995273c111f6c0307b2e109f01
paesi <97041237+paesi@users.noreply.github.com>
2025-05-04 21:28:10 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,9 @@ import { HelmetProvider } from 'react-helmet-async';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Hero from './components/Hero';
-import ServicesSection from './components/ServicesSection';
-import WhyChooseUs from './components/WhyChooseUs';
+import Elfe from './pages/Elfe';
+import Steinbruch from './pages/Steinbruch';
+import Verein from './pages/Verein';
 import Contact from './components/Contact';
 import Imprint from './pages/Imprint';
 import Privacy from './pages/Privacy';
@@ -23,20 +24,21 @@ export default function App() {
           <GoogleAnalytics />
           <Header />
           
-          <Routes>
-            <Route path="/" element={
-              <main className="flex-grow pt-[104px]">
-                <Hero />
-                <ServicesSection />
-                <WhyChooseUs />
-                <Contact />
-              </main>
-            } />
-            <Route path="/imprint" element={<Imprint />} />
-            <Route path="/privacy" element={<Privacy />} />
-            <Route path="/terms" element={<Terms />} />
-            <Route path="/thank-you" element={<ThankYouPage />} />
-          </Routes>
+  <Routes>
+    <Route path="/" element={
+      <main className="flex-grow pt-[104px]">
+        <Hero />
+        <Contact />
+      </main>
+    } />
+    <Route path="/elfe" element={<Elfe />} />
+    <Route path="/steinbruch" element={<Steinbruch />} />
+    <Route path="/verein" element={<Verein />} />
+    <Route path="/imprint" element={<Imprint />} />
+    <Route path="/privacy" element={<Privacy />} />
+    <Route path="/terms" element={<Terms />} />
+    <Route path="/thank-you" element={<ThankYouPage />} />
+  </Routes>
 
           <Footer />
         </div>
