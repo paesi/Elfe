6fd8cae24f8ea36997497418ce1c331da4006e27
paesi <97041237+paesi@users.noreply.github.com>
2025-05-08 22:02:58 +0200

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Menu, X, Phone, Mail } from 'lucide-react';
 import { NavButton } from './navigation/NavButton';
 import { MobileNavButton } from './navigation/MobileNavButton';
 import { useNavigate } from 'react-router-dom';
-import Logo from '../assets/Logo_Elfe.jpeg';
+import Logo from '../assets/Logo_Elfe.jpg';
 
 export default function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
